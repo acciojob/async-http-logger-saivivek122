8e2014b29c8a91122089fac91e9dfd2384db0706
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-02-04 06:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 async function getTodos(){
 	try{
-		let response=awat fetch("https://jsonplaceholder.typicode.com/todos/1")
+		let response=await fetch("https://jsonplaceholder.typicode.com/todos/1")
 		let data=await response.json()
 		console.log(data)
 	}
