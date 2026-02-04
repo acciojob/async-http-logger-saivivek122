9bdedc8f51ccf29fa73b0362a40131e8ce7fea11
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-02-04 06:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+async function getTodos(){
+	try{
+		let response=awat fetch("https://jsonplaceholder.typicode.com/todos/1")
+		let data=await response.json()
+		console.log(data)
+	}
+	catch(error){
+		console.log(error)
+	}
+}
+getTodos()
